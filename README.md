1283d47120cded164b151ea9b39a55983128199c
Andrew dEX <andrewdexis@gmail.com>
2014-11-08 18:09:31 +0530

    Gh init

---
+++ README.md
@@ -0,0 +1,4 @@
+BootHelper JS Framework
+========
+
+BootHelper JS Framework - This is a JS framework which has built in JS Reusable Components which can be used to easily code apps on top of of Bootstrap Front End Framework
---
+++ js/demo.js
@@ -0,0 +1,292 @@
+/*Boothelper Demo JS*/
+
+
+$(function () {
+
+
+//    boot.dLoader().Enable({
+//        loadOn:"#heading",
+//        loaderText:"BootHelper dLoader..."
+//    });
+
+
+
+
+    $("#sampleAlert").click(function () {
+
+        boot.Alert("I am the Alert", "bootHelper Alert");
+
+    });
+
+    $("#AlertBackclick").click(function () {
+
+        <!--Alert with Options-->
+        boot.Alert("I am the Alert with backclick false, you have to click the close button to close me", "bootHelper Alert", {
+            backclick: false
+        });
+
+    });
+
+
+    $("#AlertSuccess").click(function () {
+
+        <!--Alert with Success Type-->
+        boot.Alert("I am the Success Alert", "bootHelper Alert", {
+            type: 'success',
+            backclick: true
+        });
+
+
+    });
+
+
+    $("#AlertWarning").click(function () {
+
+        <!--Alert with Success Type-->
+        boot.Alert("I am the Warning Alert", "bootHelper Alert", {
+            type: 'warning',
+            backclick: true
+        });
+
+
+    });
+
+    $("#AlertInfo").click(function () {
+
+        <!--Alert with Success Type-->
+        boot.Alert("I am the info Alert", "bootHelper Alert", {
+            type: 'info',
+            backclick: true
+        });
+
+
+    });
+
+    $("#AlertError").click(function () {
+
+        <!--Alert with Success Type-->
+        boot.Alert("I am the Error Alert", "bootHelper Alert", {
+            type: 'error',
+            backclick: true
+        });
+
+
+    });
+
+    $("#sampleConfirm").click(function () {
+
+
+        <!--Simple Confirm-->
+        boot.Confirm("I am the Confirm","bootHelper Alert",function(result){
+            if(result){
+                boot.Alert("You Have Clicked Yes","BootHelper Confirm");
+            }else{
+                boot.Alert("You Have Clicked No","BootHelper Confirm");
+
+            }
+        });
+
+
+    });
+
+    $("#ConfirmOptions").click(function () {
+
+
+        <!--Simple Confirm with options-->
+        boot.Confirm("I am the Confirm","bootHelper Alert",function(result){
+                if(result){
+                    boot.Alert("You Have Clicked Yes","BootHelper Confirm");
+                }else{
+                    boot.Alert("You Have Clicked No","BootHelper Confirm");
+                }
+            },
+            {
+
+                backclick:false
+            }
+        );
+
+
+
+
+    });
+
+
+    $("#ConfirmNested").click(function () {
+
+        <!--Simple Confirm with options-->
+        boot.Confirm("I am the Confirm","bootHelper Alert",function(result){
+                if(result){
+                    <!--nested Confirm on yes click-->
+                    boot.Confirm("I am the Nested Confirm","bootHelper Alert",function(results){
+                        if(results){
+                            boot.Alert("You Have Clicked nested Yes","BootHelper Alert");
+                        }else{
+                            boot.Alert("You Have Clicked nested No","BootHelper Alert");
+                        }
+                    });
+                }else{
+                    boot.Alert("You Have Clicked No","BootHelper Confirm");
+                }
+            },
+            {
+
+                backclick:false
+            });
+
+
+
+    });
+
+
+    /*Notify Component*/
+
+
+    $("#NotifyWarning").click(function(){
+
+        boot.Notify("#notifyWrapper").Warning("Hello Warning....");
+    });
+
+
+    $("#NotifySuccess").click(function(){
+
+        boot.Notify("#notifyWrapper").Success("Hello Success....");
+    });
+
+    $("#NotifyError").click(function(){
+
+        boot.Notify("#notifyWrapper").Error("Hello Error....");
+    });
+
+    $("#NotifyInfo").click(function(){
+
+        boot.Notify("#notifyWrapper").Info("Hello Info....");
+    });
+
+
+/*dLoader */
+    $("#dLoaderEnable").click(function(){
+
+        boot.dLoader().Enable({
+            loadOn:"#wrapperLoader",
+            loaderText:"BootHelper dLoader..."
+        });
+
+
+
+    });
+
+    $("#dLoaderAjax").click(function(){
+
+        boot.dLoader().OnAjax({
+            loadOn:"#wrapperLoader",
+            loaderText:"BootHelper dLoader Ajax Loading..."
+        });
+
+        $.ajax();
+
+
+    });
+
+
+    $("#dLoaderDisable").click(function(){
+
+
+        boot.dLoader().Disable();
+
+
+    });
+
+
+    /*Html Popup*/
+    $("#HtmlPopup").click(function(){
+
+
+        boot.HtmlPopup(".modal1");
+
+
+    });
+
+
+    /*Scroll Navigation*/
+
+    $("#ScrollAlert").click(function () {
+
+        $("#ScrollConfirm").removeClass("active");
+        $("#ScrollNotify").removeClass("active");
+        $("#ScrollHtmlPopup").removeClass("active");
+        $("#ScrolldLoader").removeClass("active");
+
+        $(this).addClass("active");
+        $('html,body').animate({
+            scrollTop: $("#alertBody").offset().top -52
+        }, 1000);
+
+    });
+
+
+    $("#ScrollConfirm").click(function () {
+
+        $("#ScrollAlert").removeClass("active");
+        $("#ScrollNotify").removeClass("active");
+        $("#ScrollHtmlPopup").removeClass("active");
+        $("#ScrolldLoader").removeClass("active");
+
+        $(this).addClass("active");
+        $('html,body').animate({
+            scrollTop: $("#confirmBody").offset().top -52
+        }, 1000);
+
+    });
+
+
+    $("#ScrollNotify").click(function () {
+
+        $("#ScrollAlert").removeClass("active");
+        $("#ScrollConfirm").removeClass("active");
+        $("#ScrolldLoader").removeClass("active");
+        $("#ScrollHtmlPopup").removeClass("active");
+
+
+        $(this).addClass("active");
+        $('html,body').animate({
+            scrollTop: $("#notifyBody").offset().top -52
+        }, 1000);
+
+    });
+
+    $("#ScrolldLoader").click(function () {
+
+        $("#ScrollAlert").removeClass("active");
+        $("#ScrollConfirm").removeClass("active");
+        $("#ScrollNotify").removeClass("active");
+        $("#ScrollHtmlPopup").removeClass("active");
+
+        $(this).addClass("active");
+        $('html,body').animate({
+            scrollTop: $("#dLoaderBody").offset().top -52
+        }, 1000);
+
+    });
+
+    $("#ScrollHtmlPopup").click(function () {
+
+        $("#ScrollAlert").removeClass("active");
+        $("#ScrollConfirm").removeClass("active");
+        $("#ScrolldLoader").removeClass("active");
+        $("#ScrollNotify").removeClass("active");
+
+        $(this).addClass("active");
+        $('html,body').animate({
+            scrollTop: $("#HtmlPopupBody").offset().top -52
+        }, 1000);
+
+    });
+
+
+
+
+
+
+});
+
