36cc27ae6523005c79150b36562dee732553a9ca
Andrew dEX <andrewdexis@gmail.com>
2014-11-08 12:30:12 +0530

    Updated

--- js/demo.js
+++ js/demo.js
@@ -4,10 +4,10 @@
 $(function(){
 
 
-    boot.dLoader().Enable({
-        loadOn:"#heading",
-        loaderText:"BootHelper dLoader..."
-    });
+//    boot.dLoader().Enable({
+//        loadOn:"#heading",
+//        loaderText:"BootHelper dLoader..."
+//    });
 
     boot.Notify("#first").Warning("Hello Warning....");
     boot.Notify("#first").Error("Hello Error....");
